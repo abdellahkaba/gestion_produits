ddac54853ce83d7c1bf406836f1c0da39a5c387c
Abdellah <abdallahkaba98@gmail.com>
2025-05-01 12:46:44 +0000

    Model, Mapper, dto and Repository

---
+++ backend/src/main/java/com/isi/repository/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.isi.repository;
+
+import com.isi.model.Category;
+import com.isi.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<Product,Long> {
+
+    Optional<Product> findByName(String name) ;
+}
